66a00256130176d855f775485de90e19e24c837e
Saaho51 <dhruvtomar1210@gmail.com>
2024-09-09 16:01:33 +0530

    If_Statement.java

--- Statements/Conditional Statements/If_Statement.java
+++ Statements/Conditional Statements/If_Statement.java
@@ -1,1 +1,12 @@
 
+class If_Statement {
+    public static void main(String args[]){
+      Scanner scanner=new Scanner(System.in)
+        System.out.println("ENTERED NUMBER IS: "+num);
+        int num=scanner.nextInt();
+        if(num % 2 ==0)
+        {
+           System.out.println("The number is even ");
+        }
+    }
+}
