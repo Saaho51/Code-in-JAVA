4d49c21eb7b80925f6c776313e228ac3019e7e0d
Saaho51 <dhruvtomar1210@gmail.com>
2024-09-09 15:00:17 +0530

    If_statement.java

---
+++ Statements/Conditional/If_statement.java
@@ -0,0 +1,13 @@
+class if_statement
+  {
+    public static void main(int num)
+    {
+      System.out.println("Entered number is: "
+                         + num);
+      if(num%2==0)
+      {
+        System.out.println("The number is even");
+      }
+      System.out.println("Statement outside the If statement");
+    }
+  }
