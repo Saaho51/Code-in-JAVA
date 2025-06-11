800e9c92f9f3301d3061b3221e4130db62c44633
Saaho51 <dhruvtomar1210@gmail.com>
2025-06-11 17:38:16 +0530

    Create addition.java

---
+++ Operators/Arithematic_Operators/addition.java
@@ -0,0 +1,8 @@
+public class addition {
+  public static void main (String args[]) {
+    int x=10,y=9,z=5;
+    System.out.println("5+7 evaluates to " + (5+7));
+    System.out.println("z+12 evaluates to "+ (z+12));
+    System.out.println("x + y evaluates to "+(x + y));
+  }
+}
