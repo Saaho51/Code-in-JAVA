bab1888bb63ed3fe756f55d69fb4bb75467cb7ef
Saaho51 <dhruvtomar1210@gmail.com>
2025-07-20 21:04:12 +0530

    Update relationaloperator.java

--- relationaloperator.java
+++ relationaloperator.java
@@ -1,1 +1,14 @@
 
+public class relationaloperators 
+{
+  public static void main (String args [] ) 
+  {
+    int a = 12, b = 4 ;
+    System.out.println("a > b is " +(a>b));
+    System.out.println(" a < b is " + (a<b));
+    System.out.println("a==b is " + (a==b));
+    System.out.println("a>=b" +(a>=b));
+    System.out.println("a<=b" +(a<=b));
+    System.out.println("a!=b" +(a!=b));
+  }
+}
