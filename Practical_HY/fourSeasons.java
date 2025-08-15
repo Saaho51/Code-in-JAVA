086377dcdaf4b2d064bc7255f9bd674b2bb4beee
Saaho51 <dhruvtomar1210@gmail.com>
2025-08-15 14:47:18 +0530

    Update fourSeasons.java

--- Practical_HY/fourSeasons.java
+++ Practical_HY/fourSeasons.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+class fourSeasons {
+  public static void main (String args[]){
+    Scanner sc  = new Scanner (System.in) ;
+
+    System.out.println("Enter the month no. : ");
+    int month = scan.nextInt();
+    if (month == 4 || month==5  || month == 6  ) 
+      System.out.println("It is summer season");
+    else if (month==7||month==8||month==9)
+      System
+  }
+}
